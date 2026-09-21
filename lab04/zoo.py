7dd9fa5742e9ca80479d4810c290b8bfa5bd7c35
FCANDSKP <kaijpowell8@gmail.com>
2026-09-21 15:47:48 -0700

    Initial implementation of petting zoo

---
+++ lab04/zoo.py
@@ -0,0 +1,9 @@
+print("Welcome to the Petting Zoo!")
+print("Type 'help' to get a list of all the things you can do")
+print ()
+response = input("What would you like to do?")
+if response == "help":
+    print("Hang tight, we will be able to help soon")
+else:
+    print("Sorry, the zoo is still under construction" 🚧)
+    
